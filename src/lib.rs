4b0dfd45d7de546a6c0c09ff88cb7a947e62ffc6
Tom Hunger <tehunger@gmail.com>
2020-02-05 18:27:53 +0000

    Clean up patience sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,37 @@
 pub fn patience_argsort(v: Vec<u32>) -> Vec<u32> {
     let mut piles: Vec<Vec<u32>> = vec![vec![v[0]]]; // by index so we can do back-pointers.
+    let mut out: Vec<u32> = Vec::new();
 
     // For the backpointer we use the fact that we only ever append on the right, i.e. once a
     // column c exists the previous index will always be (c - 1), so we just need to store the
     // (c - 1).len() at the time of insertion.
-    // TODO can probably do without option by just not using c == 0;
-    let mut backpointer: Vec<Vec<Option<usize>>> = vec![vec![None]];
+    // Note that we don't keep backpointers for the first heap (see index > 0 check below)
+    let mut backpointer: Vec<Vec<usize>> = vec![];
 
     for x in v.iter().skip(1) {
-        match piles.binary_search_by(|probe| probe.last().unwrap().cmp(x)) {
-            Ok(index) => {
-                piles[index].push(*x);
-                backpointer[index].push(if index < 1 {
-                    None
-                } else {
-                    Some(piles[index - 1].len() - 1)
-                });
-            }
-            Err(index) => {
-                if piles.len() <= index {
-                    piles.push(vec![*x]);
-                    backpointer.push(vec![if index < 1 {
-                        None
-                    } else {
-                        Some(piles[index - 1].len() - 1)
-                    }]);
-                } else {
-                    piles[index].push(*x);
-                    backpointer[index].push(if index < 1 {
-                        None
-                    } else {
-                        Some(piles[index - 1].len() - 1)
-                    });
-                }
+        let index = match piles.binary_search_by(|probe| probe.last().unwrap().cmp(x)) {
+            Ok(index) => index,
+            Err(index) => index,
+        };
+        if piles.len() <= index {
+            piles.push(vec![*x]);
+            backpointer.push(vec![piles[index - 1].len() - 1]);
+        } else {
+            piles[index].push(*x);
+            if index > 0 {
+                backpointer[index - 1].push(piles[index - 1].len() - 1);
             }
         }
     }
     // Pick _a_ longest increasing subsequence, not necessarily unique
-    let mut out: Vec<u32> = Vec::new();
-    let mut i = backpointer.len() - 1;
+    let mut i = piles.len() - 1;
     let mut j = 0;
-    loop {
+    while i > 0 {
         out.push(piles[i][j]);
-        match backpointer[i][j] {
-            Some(new_j) => {
-                j = new_j;
-                i = i - 1;
-            }
-            None => break,
-        }
+        j = backpointer[i - 1][j];
+        i = i - 1;
     }
+    out.push(piles[i][j]);
     out.reverse();
     out
 }
